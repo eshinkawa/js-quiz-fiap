5d22873fc23758888da1c2a9b569505c6e7c4372
Eduardo Shinkawa <eshinkawa@gmail.com>
2024-05-09 11:25:00 -0300

    mudancas

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,26 +1,17 @@
+// Import the functions needed from the Firebase SDKs
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
-// Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBQWBD8AzoyaTgxLxG76LEZySALaVePCpo',
+  authDomain: 'js-quiz-fiap.firebaseapp.com',
+  projectId: 'js-quiz-fiap',
+  storageBucket: 'js-quiz-fiap.appspot.com',
+  messagingSenderId: '982866706469',
+  appId: '1:982866706469:web:3ae7aa183a58a23ca4bfcc',
 };
 
-const firebase = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
 
-export default firebase;
+export { database };
